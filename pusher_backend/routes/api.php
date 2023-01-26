3979f8499662652ebb4c922f2342c16f8c624bc8
imrul Afnan <imrul.afnan18@gmail.com>
2023-01-26 18:50:02 +0600

    initial pusher

---
+++ pusher_backend/routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Events\myEvent;
+use App\Http\Controllers\TestController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/test', [TestController::class, 'token']);
+Route::get('/test1', function () {
+    return event(new myEvent(['how are you?']));
+});
+Route::post('/test', [TestController::class, 'taken']);
